d62ac4583767a5f6494c62b3fc12d65d53cd2eb9
Luis Fernando <lcruzf@intercorp.com.pe>
2026-04-24 22:17:38 -0500

    update: App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         >
           Learn React
         </a>
-        <div>PROYECTO FRONT PARA PIPELINE (DESDE AWS)</div>
+        <div>PROYECTO FRONT PARA PIPELINE (DESDE JENKINS - CON VERCEL)</div>
       </header>
     </div>
   );
